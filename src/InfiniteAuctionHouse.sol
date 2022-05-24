fd3c98dfe7b4a6af92a208b2efcf15766947acc0
ghardin1314 <g.hardin1314@gmail.com>
2022-05-24 12:13:50 -0700

    create doubly linked list

--- src/InfiniteAuctionHouse.sol
+++ src/InfiniteAuctionHouse.sol
@@ -8,7 +8,7 @@ import "nouns/interfaces/INounsAuctionHouse.sol";
 
 import "forge-std/Test.sol";
 
-contract InfiniteAuctionHouse is ReentrancyGuard, Pausable, Ownable {
+contract InfiniteAuctionHouse is ReentrancyGuard, Pausable, Ownable, INounsAuctionHouse {
     // The minimum amount of time left in an auction after a new bid is created
     uint256 public timeBuffer;
 
@@ -26,17 +26,18 @@ contract InfiniteAuctionHouse is ReentrancyGuard, Pausable, Ownable {
 
     struct Bid {
         uint256 amt;
+        address prevBidder;
         address nextBidder;
     }
 
-    address public topBider;
     address internal constant SENTINEL_BID = address(0x1);
 
     mapping(address => address) bidders;
     mapping(address => Bid) public bids;
 
     constructor() {
-        topBider = SENTINEL_BID;
+        bids[SENTINEL_BID] = Bid(type(uint256).max, address(0), address(0));
+        bids[address(0)] = Bid(0, SENTINEL_BID, address(0));
     }
 
     function settleCurrentAndCreateNewAuction()
@@ -47,40 +48,74 @@ contract InfiniteAuctionHouse is ReentrancyGuard, Pausable, Ownable {
 
     function settleAuction() external nonReentrant whenPaused {}
 
-    function createBid() external payable nonReentrant {
+    function createBid(uint256 nounId) external payable nonReentrant {
         INounsAuctionHouse.Auction memory _auction = auction;
 
         // require(block.timestamp < _auction.endTime, "Auction expired");
         require(msg.value >= reservePrice, "Must send at least reservePrice");
 
-        // get top bid and set as current
-        address currentBidder = topBider;
-
-        Bid storage currentBid = bids[currentBidder];
-
-        if (msg.value > currentBid.amt) {
-            topBider = msg.sender;
-        }
-
-        console.log(currentBidder);
-        console.log(currentBid.amt);
-        console.log(currentBid.nextBidder);
-        while (msg.value < currentBid.amt) {
-            currentBidder = currentBid.nextBidder;
-            currentBid = bids[currentBidder];
-            console.log(currentBidder);
-            console.log(currentBid.amt);
-            console.log(currentBid.nextBidder);
-        }
-
-        bids[msg.sender] = Bid(msg.value, currentBidder);
+        _createBid(SENTINEL_BID);
+       
     }
 
-    /**
+        /**
      * @dev This function should be called for bids that are not top bid as a more efficient way to
      * @dev traverse the linked list by specifying the starting point
      */
-    function createBid(address prevBidder) external payable nonReentrant {}
+    function createBid(uint256 nounId, address prevBidder) external payable nonReentrant {
+
+        _createBid(prevBidder);
+
+    }
+
+    function _createBid(address prevBidder) internal {
+        // remove old bid if there is one. Recycle funds.
+        uint256 bidAmt = _deleteBid(msg.sender);
+        bidAmt += msg.value;
+        
+        console.log(bidAmt);
+
+        // get top bid and set as current
+        Bid memory prevBid = bids[prevBidder];
+        Bid memory nextBid = bids[prevBid.nextBidder];
+
+        require(bidAmt <= prevBid.amt, "Invalid previous bidder");
+
+        if (prevBidder == SENTINEL_BID && bidAmt > nextBid.amt) {
+            console.log("New Top Bid");
+        }
+
+        // Strictly less than so those who bid same amt before are first
+        while (bidAmt < nextBid.amt) {
+            prevBid = nextBid;
+            nextBid = bids[prevBid.nextBidder];
+        }
+
+        _insertBefore(prevBid.nextBidder, bidAmt);
+    }
+
+
+    function _insertBefore(address nextBidder, uint256 bidAmt) internal {
+        Bid storage nextBid = bids[nextBidder];
+        bids[msg.sender] = Bid(bidAmt, nextBid.prevBidder, nextBidder);
+        bids[nextBid.prevBidder].nextBidder = msg.sender;
+        nextBid.prevBidder = msg.sender;
+    }
+
+    function _deleteBid(address bidder) internal returns (uint256 amt) {
+        Bid memory currentBid = bids[bidder];
+
+        if(currentBid.amt == 0){
+            return 0;
+        }
+
+        amt = currentBid.amt;
+
+        bids[currentBid.prevBidder].nextBidder = currentBid.nextBidder;
+        bids[currentBid.nextBidder].prevBidder = currentBid.prevBidder;
+
+        delete(bids[bidder]);
+    }
 
     function revokeBid(uint256 nounId) external nonReentrant {}
 
--- test/InfiniteAuctionHouse.t.sol
+++ test/InfiniteAuctionHouse.t.sol
@@ -10,40 +10,163 @@ contract InfiniteAuctionHouseTest is Test {
     address internal constant SENTINEL_BID = address(0x1);
     address alice = address(2);
     address bob = address(3);
+    address charlie = address(4);
 
     function setUp() public {
         auctionHouse = new InfiniteAuctionHouse();
         deal(alice, 10 ether);
         deal(bob, 10 ether);
+        deal(charlie, 10 ether);
     }
 
     function testCreateInitialBid() public {
-        assertEq(auctionHouse.topBider(), SENTINEL_BID);
+        uint256 amt;
+        address nextBidder;
+        address prevBidder;
+
         vm.prank(alice);
-        auctionHouse.createBid{value: 1 ether}();
+        auctionHouse.createBid{value: 1 ether}(1);
 
-        assertEq(auctionHouse.topBider(), alice);
+        (, , nextBidder) = auctionHouse.bids(SENTINEL_BID);
+        assertEq(nextBidder, alice);
 
-        (uint256 amt, address nextBidder) = auctionHouse.bids(alice);
-        assertEq(nextBidder, SENTINEL_BID);
+        (amt, prevBidder, nextBidder) = auctionHouse.bids(alice);
+        assertEq(prevBidder, SENTINEL_BID);
+        assertEq(nextBidder, address(0));
         assertEq(amt, 1 ether);
+
     }
 
     function testCreateOutBid() public {
+        uint256 amt;
+        address nextBidder;
+        address prevBidder;
+
         vm.prank(alice);
-        auctionHouse.createBid{value: 1 ether}();
+        auctionHouse.createBid{value: 1 ether}(1);
 
         vm.prank(bob);
-        auctionHouse.createBid{value: 2 ether}();
+        auctionHouse.createBid{value: 2 ether}(1);
 
-        assertEq(auctionHouse.topBider(), bob);
+        (, , nextBidder) = auctionHouse.bids(SENTINEL_BID);
+        assertEq(nextBidder, bob);
 
-        (uint256 topBid, address nextBidder) = auctionHouse.bids(bob);
+        (amt, prevBidder, nextBidder) = auctionHouse.bids(bob);
+        assertEq(prevBidder, SENTINEL_BID);
         assertEq(nextBidder, alice);
-        assertEq(topBid, 2 ether);
+        assertEq(amt, 2 ether);
 
-        (uint256 lessBid, address lastBidder) = auctionHouse.bids(alice);
-        assertEq(lastBidder, SENTINEL_BID);
-        assertEq(lessBid, 1 ether);
+        (amt, prevBidder, nextBidder) = auctionHouse.bids(alice);
+        assertEq(prevBidder, bob);
+        assertEq(nextBidder, address(0));
+        assertEq(amt, 1 ether);
+    }
+
+    function testCreateReOutBid() public {
+        uint256 amt;
+        address nextBidder;
+        address prevBidder;
+
+        vm.prank(alice);
+        auctionHouse.createBid{value: 1 ether}(1);
+
+        vm.prank(bob);
+        auctionHouse.createBid{value: 2 ether}(1);
+
+        vm.prank(alice);
+        auctionHouse.createBid{value: 1.5 ether}(1);
+
+        (, , nextBidder) = auctionHouse.bids(SENTINEL_BID);
+        assertEq(nextBidder, alice);
+
+        (amt, prevBidder, nextBidder) = auctionHouse.bids(alice);
+        assertEq(prevBidder, SENTINEL_BID);
+        assertEq(nextBidder, bob);
+        assertEq(amt, 2.5 ether);
+
+        (amt, prevBidder, nextBidder) = auctionHouse.bids(bob);
+        assertEq(prevBidder, alice);
+        assertEq(nextBidder, address(0));
+        assertEq(amt, 2 ether);
+    }
+
+    function testCreateReMidBid() public {
+        uint256 amt;
+        address nextBidder;
+        address prevBidder;
+
+        vm.prank(alice);
+        auctionHouse.createBid{value: 1 ether}(1);
+
+        vm.prank(bob);
+        auctionHouse.createBid{value: 2 ether}(1);
+
+        vm.prank(alice);
+        auctionHouse.createBid{value: 0.5 ether}(1);
+
+        (, , nextBidder) = auctionHouse.bids(SENTINEL_BID);
+        assertEq(nextBidder, bob);
+
+        (amt, prevBidder, nextBidder) = auctionHouse.bids(bob);
+        assertEq(prevBidder, SENTINEL_BID);
+        assertEq(nextBidder, alice);
+        assertEq(amt, 2 ether);
+
+        (amt, prevBidder, nextBidder) = auctionHouse.bids(alice);
+        assertEq(prevBidder, bob);
+        assertEq(nextBidder, address(0));
+        assertEq(amt, 1.5 ether);
+    }
+
+    function testCreateMidSpecifiedBid() public {
+        uint256 amt;
+        address nextBidder;
+        address prevBidder;
+
+        vm.prank(alice);
+        auctionHouse.createBid{value: 1 ether}(1);
+
+        vm.prank(bob);
+        auctionHouse.createBid{value: 2 ether}(1);
+
+        vm.prank(charlie);
+        auctionHouse.createBid{value: 1.5 ether}(1, bob);
+
+        (amt, prevBidder, nextBidder) = auctionHouse.bids(bob);
+        assertEq(nextBidder, charlie);
+
+        (amt, prevBidder, nextBidder) = auctionHouse.bids(charlie);
+        assertEq(prevBidder, bob);
+        assertEq(nextBidder, alice);
+        assertEq(amt, 1.5 ether);
+
+
+        (amt, prevBidder, nextBidder) = auctionHouse.bids(alice);
+        assertEq(prevBidder, charlie);
+        assertEq(nextBidder, address(0));
+        assertEq(amt, 1 ether);
+    }
+
+    function testCreateFloorSpecifiedBid() public {
+        uint256 amt;
+        address nextBidder;
+        address prevBidder;
+
+        vm.prank(alice);
+        auctionHouse.createBid{value: 1 ether}(1);
+
+        vm.prank(bob);
+        auctionHouse.createBid{value: 2 ether}(1);
+
+        vm.prank(charlie);
+        auctionHouse.createBid{value: 0.5 ether}(1, alice);
+
+        (amt, prevBidder, nextBidder) = auctionHouse.bids(alice);
+        assertEq(nextBidder, charlie);
+
+        (amt, prevBidder, nextBidder) = auctionHouse.bids(charlie);
+        assertEq(prevBidder, alice);
+        assertEq(nextBidder, address(0));
+        assertEq(amt, 0.5 ether);
     }
 }
